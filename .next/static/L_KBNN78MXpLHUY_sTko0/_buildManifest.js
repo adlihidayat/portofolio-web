5e96712ce0d892f748dddfde1905b21fb42efd75
oneiric 30 <adlihidayat30@gmail.com>
2023-01-31 13:22:17 +0700

    first commit

---
+++ .next/static/L_KBNN78MXpLHUY_sTko0/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/996-b139276d2cf172e3.js","static/chunks/pages/index-cf51583351e30060.js"],"/_error":["static/chunks/pages/_error-8353112a01355ec2.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
